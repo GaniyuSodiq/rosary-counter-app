97a2be66cfd7a70703240c31d3bf91493e782212
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-12 06:57:09 +0100

    complete the html and css development

---
+++ app.js
@@ -0,0 +1,7 @@
+let countsValue = document.getElementById("counts-value")
+let countsBtn = document.getElementById("counts-btn")
+let countsSave = document.getElementById("counts-save")
+let countsAlert = document.getElementById("counts-alert")
+let savedCounts = document.getElementById("saved-counts")
+
+let count = 0
