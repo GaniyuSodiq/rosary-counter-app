fd16848879d14a0628a51e96aed464bd5f505346
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-12 07:22:24 +0100

    add the javascript part but couldnt make the if statment work as I need, coming back to this

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ let countsAlert = document.getElementById("counts-alert")
 let savedCounts = document.getElementById("saved-counts")
 
 let count = 0
+
+countsBtn.addEventListener("click", function(){
+    count += 1
+    countsValue.textContent = count
+    return count
+})
+
+countsSave.addEventListener("click", function(){
+    savedCounts.textContent += count + " - "
+    count = 0
+    countsValue.textContent = 0
+    return count
+})
+
+// if (count = 33) {
+//     countsAlert.textContent = "You have made 33 counts"
+// } else if (count = 100) {
+//     countsAlert.textContent = "You have made 100 counts"
+// } else {
+//     countsAlert.textContent = ""
+// }
